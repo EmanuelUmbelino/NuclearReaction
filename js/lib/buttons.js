4d23e6338c49859cda33c4378d98cd5b3a1e2b6c
EmanuelUmbelino <emanuel.umbelino@hotmail.com>
2015-11-25 14:20:31 -0200

    Version 0.8
    - Add Progressive Dificult
    - Sound Button (on/off)

--- js/lib/buttons.js
+++ js/lib/buttons.js
@@ -3,40 +3,48 @@ var Buttons = function(){
 	this.x = 400;
 	
 	this.playY = 250;
-	this.playPivotY = this.playY - 50
+	this.playPivotY = this.playY - 50;
 	this.playWidth = 130;
 	this.playHeight = 70;
-
-	this.easyY = 230;
-    this.easyPivotY = this.easyY - 50
-    this.easyWidth = 110;
-    this.easyHeight = 50;
-	
-	this.meadiumY = 340;
-    this.meadiumPivotY = this.meadiumY - 50
-    this.meadiumWidth = 180;
-    this.meadiumHeight = 60;
-	
-	this.hardY = 450;
-    this.hardPivotY = this.hardY - 50
-    this.hardWidth = 115;
-    this.hardHeight = 55;
 	
 	this.directionY = 350;
-    this.directionPivotY = this.directionY - 50
+    this.directionPivotY = this.directionY - 50;
     this.directionWidth = 400;
     this.directionHeight = 60;
 
 	this.creditsY = 450;
-    this.creditsPivotY = this.creditsY - 50
+    this.creditsPivotY = this.creditsY - 50;
     this.creditsWidth = 230;
     this.creditsHeight = 50;
+	
+	this.soundX = 700;
+	this.soundY = 550;
+    this.soundPivotY = this.soundY - 30;
+	this.soundWidth = 130;
+	this.soundHeight = 80;
+	this.soundColor = "Green";
+
+	this.easyY = 230;
+    this.easyPivotY = this.easyY - 50;
+    this.easyWidth = 110;
+    this.easyHeight = 50;
+	
+	this.meadiumY = 340;
+    this.meadiumPivotY = this.meadiumY - 50;
+    this.meadiumWidth = 180;
+    this.meadiumHeight = 60;
+	
+	this.hardY = 450;
+    this.hardPivotY = this.hardY - 50;
+    this.hardWidth = 115;
+    this.hardHeight = 55;
 
 	this.draw = function() 
 	{
 		graphics.drawText(this.x, this.playY, "70px", "Play", "Green");
 		graphics.drawText(this.x, this.directionY, "70px", "Instructions", "Green");
 		graphics.drawText(this.x, this.creditsY, "70px", "Credits", "Green");
+		graphics.drawText(this.soundX, this.soundY, "40px", "Sound", this.soundColor);
 
 		if(mouse.x > this.x - this.playWidth/2 && mouse.y > this.playPivotY && mouse.x < this.x + this.playWidth/2 && mouse.y < this.playPivotY + this.playHeight)
 		{ 
@@ -48,7 +56,12 @@ var Buttons = function(){
 				atom.y = (graphics.canvas.height / 2);
 			}
 		}
-
+		if(mouse.x > this.soundX - this.soundWidth/2 && mouse.y > this.soundPivotY && mouse.x < this.soundX + this.soundWidth/2 && mouse.y < this.soundPivotY + this.soundHeight/2)
+		{ 
+			graphics.drawCicle(this.soundX - this.soundWidth/2 - 20, this.soundPivotY + 15,10, this.soundColor) 
+			if(this.soundColor == "Green" && mouse.click){ this.soundColor = "Red";sound.music.pause();}
+			else if(mouse.click){this.soundColor = "Green";sound.music.play();}
+		}
 		if(mouse.x > this.x - this.directionWidth/2 && mouse.y > this.directionPivotY && mouse.x < this.x + this.directionWidth/2 && mouse.y < this.directionPivotY + this.creditsHeight)
 		{ 
 			graphics.drawCicle(this.x - this.directionWidth/2 - 20, this.directionPivotY + 30,10, "GREEN"); 
--- js/lib/sound.js
+++ js/lib/sound.js
@@ -3,7 +3,7 @@ var Sound = function()
 	this.music = new Audio();
 	this.music.src = "./mp3/mozard.mp3";
 
-	this.music.volume = 0;
+	this.music.volume = 1;
 	this.music.play();
 	
 	this.update = function()
